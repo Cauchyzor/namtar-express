61775ad46da2b0b60837a52d0b03770d5650d96b
Freedomjail <f.pinzan@hotmail.com>
2020-04-14 23:53:20 +0200

    Générateur de créature fix

--- routes/species.js
+++ routes/species.js
@@ -3,20 +3,26 @@ var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 
 router.get('/', function (req, res) {
-  var QUERY_STRING = "SELECT * FROM TYPE;";
+
+  var QUERY_TYPE = "SELECT * FROM TYPE;";
+
   var db = new sqlite3.Database('speciesGenerator.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
   });
-  db.all(QUERY_STRING, function (err, rows) {
+
+  db.all(QUERY_TYPE, function (err, rows) {
     res.render('species', { species: "AJAX is great!", especes_list: rows });
+    console.log(rows);
   });
+
   db.close((err) => {
     if (err) {
       return console.error(err.message);
     }
   });
+
 });
 
 module.exports = router;
