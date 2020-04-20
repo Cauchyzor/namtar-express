a9217f4cf2bff92f35fc767db38d8a09e4746f86
Freedomjail <f.pinzan@hotmail.com>
2020-04-20 17:42:16 +0200

    Factorisation du code du bestiaire

--- routes/bestiaire.js
+++ routes/bestiaire.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
 
-/* GET home page. */
+var async = require('async');
+
 router.get('/', function(req, res, next) {
-  res.render('bestiaire', { title: 'Express' });
+
+	var db = new sqlite3.Database('speciesGenerator.db', (err) => {
+		if (err) {
+			return console.error(err.message);
+		}
+	});
+
+	var QUERY_ESPECE = "SELECT * FROM ESPECE";
+	var QUERY_RACE = "SELECT * FROM RACE";
+
+	async.series({
+
+		espece: function(cb) {
+            db.all(QUERY_ESPECE, function (error, rows){
+                cb(error, rows);
+            })
+        },
+        race: function(cb){
+            db.all(QUERY_RACE, function (error, rows){
+                cb(error, rows)
+            })
+        }
+    	
+	}, function(error, rows) {
+		if (!error) {
+			res.render('bestiaire', {table_list: rows});
+		}
+
+    });
+
+
 });
 
 module.exports = router;
