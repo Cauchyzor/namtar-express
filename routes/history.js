1cb62c390618e0f2d934713083e8bab830274796
Cauchyzor <cauchyzor@gmail.com>
2020-11-21 16:42:25 +0100

    Rename for homogne language 'histoire'

--- routes/history.js
+++ routes/history.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('histoire', { title: 'Express' });
+  res.render('history', { title: 'Express' });
 });
 
 module.exports = router;
