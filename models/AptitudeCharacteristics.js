4ba91a5ad01a8e1ff613a66a508975e2e24fafe3
Cauchyzor <pierrot.cauchy@gmail.com>
2021-01-11 20:36:45 +0100

    Modularisation Apt informations

---
+++ models/AptitudeCharacteristics.js
@@ -0,0 +1,29 @@
+const aptCharMap = new Map();
+
+aptCharMap.set('Athletisme', 'Vigueur');
+aptCharMap.set('Pugilat', 'Vigueur');
+aptCharMap.set('Coercion', 'Vigueur');
+aptCharMap.set('Corps  à corps', 'Vigueur');
+aptCharMap.set('Résistance', 'Vigueur');
+aptCharMap.set('Coordination', 'Agilité');
+aptCharMap.set('Artillerie', 'Agilité');
+aptCharMap.set('Pilotage', 'Agilité');
+aptCharMap.set('Distance - Légère', 'Agilité');
+aptCharMap.set('Distance - Lourde', 'Agilité');
+aptCharMap.set('Discrétion', 'Agilité');
+aptCharMap.set('Informatique', 'Intelligence');
+aptCharMap.set('Mécanique', 'Intelligence');
+aptCharMap.set('Medecine', 'Intelligence');
+aptCharMap.set('Résonance', 'Intelligence');
+aptCharMap.set('Perception', 'Ruse');
+aptCharMap.set('Magouille', 'Ruse');
+aptCharMap.set('Survie', 'Ruse');
+aptCharMap.set('Calme', 'Volonté');
+aptCharMap.set('Sang-froid', 'Volonté');
+aptCharMap.set('Vigilance', 'Volonté');
+aptCharMap.set('Charme', 'Présence');
+aptCharMap.set('Tromperie', 'Présence');
+aptCharMap.set('Negociation', 'Présence');
+aptCharMap.set('Commandement', 'Présence');
+
+module.exports = aptCharMap;
---
+++ models/AptitudesAttributes.js
@@ -0,0 +1,29 @@
+const aptTypeMap = new Map();
+
+aptTypeMap.set('Athletisme', 'Général');
+aptTypeMap.set('Pugilat', 'Combat');
+aptTypeMap.set('Coercion', 'Général');
+aptTypeMap.set('Corps  à corps', 'Comabt');
+aptTypeMap.set('Résistance', 'Général');
+aptTypeMap.set('Coordination', 'Utilitaire');
+aptTypeMap.set('Artillerie', 'Combat');
+aptTypeMap.set('Pilotage', 'Utilitaire');
+aptTypeMap.set('Distance - Légère', 'Combat');
+aptTypeMap.set('Distance - Lourde', 'Combat');
+aptTypeMap.set('Discrétion', 'Utilitaire');
+aptTypeMap.set('Informatique', 'Utilitaire');
+aptTypeMap.set('Mécanique', 'Utilitaire');
+aptTypeMap.set('Medecine', 'Utilitaire');
+aptTypeMap.set('Résonance', 'Combat');
+aptTypeMap.set('Perception', 'Général');
+aptTypeMap.set('Magouille', 'Utilitaire');
+aptTypeMap.set('Survie', 'Utilitaire');
+aptTypeMap.set('Calme', 'Général');
+aptTypeMap.set('Sang-froid', 'Général');
+aptTypeMap.set('Vigilance', 'Général');
+aptTypeMap.set('Charme', 'Social');
+aptTypeMap.set('Tromperie', 'Social');
+aptTypeMap.set('Negociation', 'Social');
+aptTypeMap.set('Commandement', 'Social');
+
+module.exports = aptTypeMap;
