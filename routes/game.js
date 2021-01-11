eb2b91ed8ea69222fe6d73d49ca277fc71138570
Cauchyzor <pierrot.cauchy@gmail.com>
2021-01-11 20:35:33 +0100

    remove debug instructions

--- routes/game.js
+++ routes/game.js
@@ -45,20 +45,17 @@ router.post('/comment/', function (req, res, next) {
 });
 
 router.get('/stateEditor', function (req, res, next) {
-  console.log(req.query);
   State.findOne({ _id: req.query._id })
     .then((state) => { res.render('gameStateEditor', { state: state }); })
     .catch((error) => { res.status(400).json({ error: error }); });
 });
 
 router.post('/stateEditor', function (req, res, next) {
-  console.log(req.body);
   State.updateOne({ _id: req.body.stateId }, { title: req.body.stateTitle, body: req.body.stateContent }, function (res, err) {
     // if (res !== null) console.log(res);
     // if (err !== null) console.log(err);
   });
   res.redirect('/game/selection');
 });
-
 
 module.exports = router;
