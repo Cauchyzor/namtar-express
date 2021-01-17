2eb0eee8f545152d45618e2d6bcd249abf5de8e7
Cauchyzor <pierrot.cauchy@gmail.com>
2021-01-17 10:54:57 +0100

    Fix unclean syntaxe

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    console.log(req.session);
     const decodeToken = jwt.verify(req.session.token, 'RANDOM_SECRET');
     const userId = decodeToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
+      // eslint-disable-next-line no-throw-literal
       throw 'User ID non valable !';
     } else {
-      console.log('Décoded !');
-      req.user = userId;
+      req.userId = userId;
       next();
     };
   } catch (error) {
