ac8e8baf8863adf389d6dbbcae715997fdcdc34f
Cauchyzor <pierrot.cauchy@gmail.com>
2021-02-03 20:07:28 +0100

    Init GM pages for administration

--- models/Game.js
+++ models/Game.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const gameSchema = mongoose.Schema({
   name: { type: String, required: true },
-  charactersPlaying: [{ type: mongoose.Types.ObjectId, ref: 'Character' }]
+  charactersPlaying: [{ type: mongoose.Types.ObjectId, ref: 'Character' }],
+  owner: { type: mongoose.Types.ObjectId, ref: 'User' }
 },
 {
   timestamps: true
--- routes/game.js
+++ routes/game.js
@@ -6,20 +6,11 @@ const Comment = require('../models/Comment');
 const Game = require('../models/Game');
 const Character = require('../models/Character');
 
-// TODO: Trouver une requete filtre sur Game et User qui remonte les games et les Characters
-
 router.get('/selection', function (req, res, next) {
   Game.find()
     .populate('charactersPlaying')
     .then(games => res.render('gameSelection', { games: games }))
     .catch(error => res.status(400).json({ error }));
-});
-
-router.post('/selection', auth, function (req, res, next) {
-  const game = new Game({ name: req.body.gameName });
-  game.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-    .catch(error => res.status(500).json({ error }));
 });
 
 router.get('/engine', auth, function (req, res, next) {
@@ -34,13 +25,6 @@ router.get('/engine', auth, function (req, res, next) {
     .catch(error => res.status(400).json({ error }));
 });
 
-router.post('/createState', auth, function (req, res, next) {
-  const newStats = new State({ title: req.body.stateTitle, gameId: req.query._id });
-  newStats.save()
-    .then(() => res.status(201).json({ message: 'State enregistré !' }))
-    .catch(error => res.status(500).json({ error }));
-});
-
 router.post('/comment/', function (req, res, next) {
   const newComment = new Comment({ body: req.body.commentBody, characterPostingId: req.body.posterId });
   newComment.save()
@@ -49,15 +33,48 @@ router.post('/comment/', function (req, res, next) {
     .catch(error => res.status(500).json({ error }));
 });
 
-router.get('/stateEditor', auth, function (req, res, next) {
-  State.findOne({ _id: req.query._id })
-    .then(state => res.render('gameStateEditor', { state: state }))
+router.get('/owner/selection', auth, function (req, res, next) {
+  Game.find({ owner: req.userId })
+    .populate('charactersPlaying')
+    .then(games => res.render('gameOwnerSelection', { games: games }))
     .catch(error => res.status(400).json({ error }));
 });
 
-router.post('/stateEditor', auth, function (req, res, next) {
+router.post('/owner/selection', auth, function (req, res, next) {
+  const game = new Game({ name: req.body.gameName, owner: req.userId });
+  game.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.get('/owner/engine', auth, function (req, res, next) {
+  let states;
+  State.find({ gameId: req.query.gameId })
+    .populate('comments')
+    .sort('-createdAt')
+    .then(premiseStates => { states = premiseStates; return Game.findById(req.query.gameId).populate('charactersPlaying'); })
+    .then(premiseGame => res.render('gameOwnerEngine', { states: states, game: premiseGame }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.post('/owner/createState', auth, function (req, res, next) {
+  const newStats = new State({ title: req.body.stateTitle, gameId: req.query._id });
+  newStats.save()
+    .then(() => res.status(201).json({ message: 'State enregistré !' }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.get('/owner/stateEditor', auth, function (req, res, next) {
+  let state;
+  State.findOne({ _id: req.query._id })
+    .then(statePremise => { state = statePremise; Game.exists({ owner: req.userId }); })
+    .then(() => res.render('gameStateEditor', { state: state }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.post('/owner/stateEditor', auth, function (req, res, next) {
   State.updateOne({ _id: req.body.stateId }, { title: req.body.stateTitle, body: req.body.stateContent })
-    .then(() => res.redirect('/game/selection'))
+    .then(() => res.redirect('/game/owner/selection'))
     .catch(error => res.status(400).json({ error }));
 });
 
